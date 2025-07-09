615bb9901f1161ac86c45db3b1f53946ea091982
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2025-07-09 12:14:59 +0200

    updated types

--- client/src/components/wallet-login.tsx
+++ client/src/components/wallet-login.tsx
@@ -3,10 +3,12 @@ import {JSX} from 'react';
 import {WalletSheet} from '@/components/navbar/wallet-sheet';
 import {NetworkSelectButton} from '@/components/network-select-button';
 import {useSession} from 'next-auth/react';
+import {useAppKitAccount} from '@reown/appkit/react';
 
 export const WalletLogin = (): JSX.Element => {
 	const {data: session} = useSession();
-	if (session?.address) {
+	const {isConnected} = useAppKitAccount();
+	if (session?.address && isConnected) {
 		return (
 			<>
 				<NetworkSelectButton/>
@@ -15,6 +17,6 @@ export const WalletLogin = (): JSX.Element => {
 		);
 	}
 	
-	return <appkit-connect-button size="sm" label="Connect login" loadingLabel="Loading..."/>;
+	return <appkit-connect-button size="sm" label="Connect wallet" loadingLabel="Loading..."/>;
 	
 };
